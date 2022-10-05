21bdf6757fcc67add57cabdc9014d343ffc9d068
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-10-05 15:26:02 -0300

    Finaliza implementação grpc

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ docker-compose up -d
 docker-compose exec app sh
 ```
 
-3. Caso queira gerar novamente as stubs:
+3. Para realizar um teste, dentro do container em uma aba do terminal execute o server, e em outra aba execute o client:
+```
+go run cmd/server/server.go
+go run cmd/client/client.go
+```
+
+4. Caso queira gerar novamente as stubs:
 ```
 protoc --proto_path=proto/ proto/*.proto --plugin=$(go env GOPATH)/bin/protoc-gen-go-grpc --go-grpc_out=. --go_out=.
+
--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/LeonardoVales/go-aluno-grpc/pb"
 	"google.golang.org/grpc"
@@ -19,7 +20,9 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
-	AddUserVerbose(client)
+	// AddUserVerbose(client)
+	// AddUsers(client)
+	AddUserStreamBoth(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -61,4 +64,124 @@ func AddUserVerbose(client pb.UserServiceClient) {
 
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "w1",
+			Name:  "Leonardo",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w2",
+			Name:  "Leonardo 2",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w3",
+			Name:  "Leonardo 3",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w4",
+			Name:  "Leonardo 4",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w5",
+			Name:  "Leonardo 5",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w6",
+			Name:  "Leonardo 6",
+			Email: "leo@gmail.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
+}
+
+func AddUserStreamBoth(client pb.UserServiceClient) {
+	stream, err := client.AddUserStreamBoth(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "w1",
+			Name:  "Leonardo",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w2",
+			Name:  "Leonardo 2",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w3",
+			Name:  "Leonardo 3",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w4",
+			Name:  "Leonardo 4",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w5",
+			Name:  "Leonardo 5",
+			Email: "leo@gmail.com",
+		},
+		&pb.User{
+			Id:    "w6",
+			Name:  "Leonardo 6",
+			Email: "leo@gmail.com",
+		},
+	}
+
+	wait := make(chan int)
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending user: ", req.GetName())
+			stream.Send(req)
+			time.Sleep(time.Second * 3)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error receiving data: %v", err)
+				break
+			}
+			fmt.Printf("Recebendo user %v com status: %v\n", res.GetUser().GetName(), res.GetStatus())
+		}
+		close(wait)
+	}()
+
+	<-wait
 }
